ba680092b2a475b6687b56f069537af0abbbaa80
bitechnologymx2 <35075832+bitechnologymx2@users.noreply.github.com>
2018-02-20 16:55:24 -0600

    ..

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AfterContentInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
-
+export class HomePage implements AfterContentInit {
   constructor(public navCtrl: NavController) {
 
   }
+  ngAfterContentInit() {
 
+    var wh = $(window).height()+1;
+    var ww = $(window).width()+1;
+
+    $("#textoIngresar").css("color", "white");
+    //$("#video-background").css('width', (ww+'px'));
+    //$("#video-background").css('height', ((wh+45)+'px'));
+    $('#video-background').attr('playsinline','');
+    //$("#video-background").css({'width':(ww+'px'),'height':((wh+45)+'px')});
+    let video = document.getElementById("video-background");
+    //video.play();
+  }
 }
